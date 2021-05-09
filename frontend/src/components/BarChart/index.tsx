4e64e7ddde1010c9583c1e17fa1779a29e0a4419
jeanfurquim <jean.furquim@jpisistemas.com.br>
2021-05-08 21:50:09 -0300

    DATABLE INTEGRATION

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useEffect } from 'react'
 import { useState } from 'react'
 import Chart from 'react-apexcharts'
-import { SaleSucess, SaleSum } from 'types/sale'
+import { SaleSucess } from 'types/sale'
 import { round } from 'utils/format'
 import { BASE_URL } from 'utils/requests'
 
